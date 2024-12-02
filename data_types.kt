7acd5fbb358ac3081433261fdf9fd847883ab8b5
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-02 21:01:03 +0900

    Create data_types.kt

---
+++ data_types.kt
@@ -0,0 +1,27 @@
+fun main() {
+    // Declaration
+    val constant01: Int = 5
+    val constant02: Double = 3.14
+    val constant03: Char = 'D'
+    val constant04: Boolean = true
+    val constant05: String = "Hello"
+
+    println(constant01)
+    println(constant02)
+    println(constant03)
+    println(constant04)
+    println(constant05)
+
+    // Integers
+    val integer01: Byte = -128 // -128 ~ 127
+    val integer02: Short = -32768 // -32768 ~ 32767
+    val integer03: Int = -2147483648 // -2147483648 ~ 2147483647
+    val integer04: Long = -9999999999999 // -9223372036854775808 ~ 9223372036854775807
+
+    // Floating Point Numbers
+    val float01: Float = 3.14F
+    val float02: Double = 3.141592
+    val float03: Float = 3.14E2F
+    val float04: Double = 3.141592E2
+
+}
