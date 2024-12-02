b3a0eeb96eee4415b0bda6ad4b91448bce326a04
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-02 20:48:05 +0900

    Create variables.kt

---
+++ variables.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // Declaration
+    var variable = "Alphabet"
+    val constant = "Alphabet"
+    println(variable)
+    println(constant)
+
+    variable = "alphabet"
+    println(variable)
+}
