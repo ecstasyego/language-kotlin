7bc17f2fcabbf0f2eab3c915517551def7ce4b63
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-03 11:27:16 +0900

    Create loop-for.kt

---
+++ basic/loop-for.kt
@@ -0,0 +1,20 @@
+for (i in 1..10) {
+    println(i)
+}
+
+for (i in 1..10 step 1) {
+    println(i)
+}
+
+for (i in 1 until 11) {
+    println(i)
+}
+
+for (i in 1 until 11 step 1) {
+    println(i)
+}
+
+# reverse
+for (i in 10 downTo 1) {
+    println(i)
+}
