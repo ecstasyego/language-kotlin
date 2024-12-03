52d7d8690f90aad2203414c4c013eefa87d660ba
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-03 11:54:47 +0900

    Update data_types.kt

--- basic/data_types.kt
+++ basic/data_types.kt
@@ -1,24 +1,27 @@
-// Declaration
-val constant01: Int = 5
-val constant02: Double = 3.14
-val constant03: Char = 'D'
-val constant04: Boolean = true
-val constant05: String = "Hello"
+false is Boolean
+false::class
+false::class.simpleName
 
-println(constant01)
-println(constant02)
-println(constant03)
-println(constant04)
-println(constant05)
+1 is Int
+1::class
+1::class.simpleName
 
-// Integers
-val integer01: Byte = -128 // -128 ~ 127
-val integer02: Short = -32768 // -32768 ~ 32767
-val integer03: Int = -2147483648 // -2147483648 ~ 2147483647
-val integer04: Long = -9999999999999 // -9223372036854775808 ~ 9223372036854775807
+10L is Long
+10L::class
+10L::class.simpleName
 
-// Floating Point Numbers
-val float01: Float = 3.14F
-val float02: Double = 3.141592
-val float03: Float = 3.14E2F
-val float04: Double = 3.141592E2
+10F is Float
+10F::class
+10F::class.simpleName
+
+10E0 is Double
+10E0::class
+10E0::class.simpleName
+
+'A' is Char
+'A'::class
+'A'::class.simpleName
+
+"Alphabet" is String
+"Alphabet"::class
+"Alphabet"::class.simpleName
