fefeab2b3df0594807a21548aa17de155a6eb69f
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-07 12:44:31 +0900

    Update class-reference.kt

--- basic/class-reference.kt
+++ basic/class-reference.kt
@@ -1,7 +1,12 @@
-class Person(var name: String, var age: Int)
+class Person(var name: String, var age: Int){
+    fun method01(){}
+    fun method02(){}
+}
 
 val person = Person("Jhon", 30)
 Person::class
+Person::method01
+Person::method02
 Person::name.get(person)
 Person::age.get(person)
 Person::name.set(person, "Martin")
