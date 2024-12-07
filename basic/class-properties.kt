d7f332d2df8a034de72a558797b3c294928a2783
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-07 09:42:11 +0900

    Create class-properties.kt

---
+++ basic/class-properties.kt
@@ -0,0 +1,8 @@
+class Structure(){
+    val immutable_property01:String? = null
+    val immutable_property02:Double? = null
+    var mutable_property01:String? = null
+    var mutable_property02:Double? = null
+}
+
+Structure()
