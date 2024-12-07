b7e8f32f4c0360e22913e9f6c7c746ae83d6703e
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-07 20:14:43 +0900

    Create class-initialization.kt

---
+++ basic/class-initialization.kt
@@ -0,0 +1,13 @@
+class Structure(a:String, b:String){
+    init {
+        println(a)
+        println(b)
+    }
+
+    init {
+        println("Good")
+        println("Bye")
+    }
+}
+
+Structure("Hello", "World")
