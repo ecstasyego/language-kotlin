ebbbd2019c5e041211c4bcb637a463eb308aa371
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-07 13:10:31 +0900

    Update function-lambda.kt

--- basic/function-lambda.kt
+++ basic/function-lambda.kt
@@ -1,5 +1,10 @@
-fun function(x:Int, y:Int):Int {return x * y}
-fun lambda_func(x:Int, y:Int) = x * y
+fun function01(x:Int, y:Int):Int {return x * y}
+fun function02(x:Int, y:Int) = x * y
 
-function(3, 2)
-lambda_func(3, 2)
+function01(3, 2)
+function02(3, 2)
+
+val lambda01 = ::function01
+val lambda02 = ::function01
+lambda01(3, 2)
+lambda02(3, 2)
