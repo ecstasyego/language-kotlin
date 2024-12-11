bdd545850e93c081b2a2338d3de43293a56da9bc
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-11 11:47:01 +0900

    Update array-arrayof.kt

--- basic/array-arrayof.kt
+++ basic/array-arrayof.kt
@@ -1,1 +1,16 @@
-var a = arrayOf(0,1,2,3,4,5)
+val arr = arrayOf(0,1,2,3,4,5)
+arr[0] = 100
+arr[1] = 100
+arr[2] = 100
+arr[3] = 100
+arr[4] = 100
+arr[5] = 100
+arr.size
+arr.joinToString()
+
+for (idx in 0..arr.size) {println(idx)}
+for (element in arr) {println(element)}
+
+arr.forEach{println(it)}
+arr.forEach{element -> println(element)}
+arr.map{it.toDouble()}.toDoubleArray()
