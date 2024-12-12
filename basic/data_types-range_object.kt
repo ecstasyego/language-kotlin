1729ec1f001d61b3e29a03658bea1195f987f3f2
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-12 20:52:57 +0900

    Update data_types-range_object.kt

--- basic/data_types-range_object.kt
+++ basic/data_types-range_object.kt
@@ -1,5 +1,15 @@
-// range object
+// range objects
 1..10
 1..<11
 1 until 11
 10 downTo 1
+
+
+// mapper: range > listOf > mapOf
+val mapper = (0..4).toList().zip(listOf("A", "B", "C", "D", "E")).toMap()
+
+val keys = listOf(0, 1, 4, 4, 3, 1, 3, 2, 4, 1, 1, 0, 0)
+val values = MutableList<String?>(keys.size){null}
+
+(0..<keys.size).forEach{ it -> values[it] = mapper[keys[it]] }
+values
