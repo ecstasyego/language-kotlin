dfd6fbc62574d2a000ff9e345a4ed6a19a2c5bee
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-12 21:28:09 +0900

    Update function-lambda.kt

--- basic/function-lambda.kt
+++ basic/function-lambda.kt
@@ -1,14 +1,18 @@
 fun function01(x:Int, y:Int):Int {return x * y}
 fun function02(x:Int, y:Int) = x * y
 val function03: (Int, Int) -> Int = { x, y -> x * y }
+val function04 = { 6 }
 
 function01(3, 2)
 function02(3, 2)
 function03(3, 2)
+function04()
 
 val lambda01 = ::function01
 val lambda02 = ::function02
 val lambda03 = ::function03
+val lambda04 = ::function04
 lambda01(3, 2)
 lambda02(3, 2)
-lambda03(3, 2)
+lambda03()(3, 2)
+lambda04()()
