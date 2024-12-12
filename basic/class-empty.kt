928d440fad34fd32bfef557909bf3349ce59377e
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-12 11:17:24 +0900

    Update class-empty.kt

--- basic/class-empty.kt
+++ basic/class-empty.kt
@@ -2,11 +2,18 @@ class Structure01
 
 class Structure02(){}
 
-class Structure03()
+class Structure03 constructor() {}
 
-class Structure04{}
+class Structure04()
+
+class Structure05 constructor()
+
+class Structure06{}
+
 
 Structure01()
 Structure02()
 Structure03()
 Structure04()
+Structure05()
+Structure06()
