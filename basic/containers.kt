f9fca2bf50e3c61c937ed12ecb4ed893d4decb9c
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-13 02:24:55 +0900

    Update containers.kt

--- basic/containers.kt
+++ basic/containers.kt
@@ -1,1 +1,15 @@
+val mlist = mutableListOf<Any>()
+mlist.add(0)
+mlist.add('A')
+mlist.add("Alphabet")
+mlist[0]
+mlist[1]
+mlist[2]
+
+val mmap = mutableMapOf<String, Any>()
+mmap["A"] = 0
+mmap["B"] = 1
+mmap["C"] = "Alphabet"
+
+
 
