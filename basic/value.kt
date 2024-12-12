211d1bf8d650692e815a17d933d4c6cacc6d0941
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-12 23:55:46 +0900

    Update value.kt

--- basic/value.kt
+++ basic/value.kt
@@ -1,5 +1,12 @@
+// Initialization
 val str01:String = "Hello, World!" // initialization
 val str02:String by lazy {"Hello, World!"} // not yet initalization
 
+str01
 str02 // initialization
-str02 
+
+
+// Null Safety
+val str03:String? = "Hello, World!" // nullable, null safety
+val str04:String = str03 !! // assertion: not-null 
+
