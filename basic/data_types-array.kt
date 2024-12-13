e19287f7e16234d2ec0caf36d12f8aa844a99eea
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-13 10:09:21 +0900

    Update data_types-array.kt

--- basic/data_types-array.kt
+++ basic/data_types-array.kt
@@ -40,6 +40,78 @@ charArrayOf('a', 'b', 'c', 'd')
 booleanArrayOf(true, false)
 
 
+// 1D Array
+Array(3) { false }
+Array(3) { 0 }
+Array(3) { 0.0 }
+
+Array(3) { row -> false }
+Array(3) { row -> 0 }
+Array(3) { row -> 0.0 }
+
+Array<Boolean>(3) { false }
+Array<Int>(3) { 0 }
+Array<Double>(3) { 0.0 }
+
+Array<Boolean>(3) { row -> false }
+Array<Int>(3) { row -> 0 }
+Array<Double>(3) { row -> 0.0 }
+
+BooleanArray(3)
+IntArray(3)
+DoubleArray(3)
+
+// 2D Array
+Array(3) { BooleanArray(3) }
+Array(3) { IntArray(3) }
+Array(3) { DoubleArray(3) }
+
+Array(3) { Array(3) { false } }
+Array(3) { Array(3) { 0 } }
+Array(3) { Array(3) { 0.0 } }
+
+Array(3) { Array<Boolean>(3) { false } }
+Array(3) { Array<Int>(3) { 0 } }
+Array(3) { Array<Double>(3) { 0.0 } }
+
+Array(3) { row -> Array(3) { col -> false } }
+Array(3) { row -> Array(3) { col -> 0 } }
+Array(3) { row -> Array(3) { col -> 0.0 } }
+
+
+// 1D Random Array
+import kotlin.random.Random
+
+Array(10) {Random.nextBoolean()}
+Array(10) {Random.nextInt(0, 10)}
+Array(10) {Random.nextDouble(-3.14, 3.14)}
+
+BooleanArray(10) { Random.nextBoolean() }
+IntArray(10) {Random.nextInt(0, 10)}
+DoubleArray(10) { Random.nextDouble(-3.14, 3.14) }
+
+Array<Boolean>(10) {Random.nextBoolean()}
+Array<Int>(10) {Random.nextInt(0, 10)}
+Array<Double>(10) {Random.nextDouble(-3.14, 3.14)}
+
+// 2D Random Array
+Array(3) { BooleanArray(3) {Random.nextBoolean()} }
+Array(3) { IntArray(3) {Random.nextInt(0, 10)} }
+Array(3) { DoubleArray(3) {Random.nextDouble(-3.14, 3.14)} }
+
+Array(3) { Array(3) { Random.nextBoolean() } }
+Array(3) { Array(3) { Random.nextInt(0, 10) } }
+Array(3) { Array(3) { Random.nextDouble(-3.14, 3.14) } }
+
+Array(3) { Array<Boolean>(3) { Random.nextBoolean() } }
+Array(3) { Array<Int>(3) { Random.nextInt(0, 10) } }
+Array(3) { Array<Double>(3) { Random.nextDouble(-3.14, 3.14) } }
+
+Array(3) { row -> Array(3) { col -> Random.nextBoolean() } }
+Array(3) { row -> Array(3) { col -> Random.nextInt(0, 10) } }
+Array(3) { row -> Array(3) { col -> Random.nextDouble(-3.14, 3.14) } }
+
+
 fun process():Array<Char>{
     return arrayOf('a', 'b', 'c')
 }
