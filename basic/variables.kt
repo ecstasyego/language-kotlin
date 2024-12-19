2554857afa5237c1dce855b20160bd0b05f5e907
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-19 18:07:01 +0900

    Update variables.kt

--- basic/variables.kt
+++ basic/variables.kt
@@ -10,6 +10,6 @@ println(variable)
 
 // Initialization
 lateinit var late_variable:String // ?(nullsafety): not working  
-val late_value by lazy {"World"}  
+val late_value:String by lazy {"World"}  
 late_variable = "Hello" // Initialization working for non-primative types such as String not Char, Int, Double
 late_value // Initialization
