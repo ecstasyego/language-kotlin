2fabd224eca4628dab3ccba1a806171db7876580
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-20 12:59:59 +0900

    Update class-constructor.kt

--- basic/class-constructor.kt
+++ basic/class-constructor.kt
@@ -1,31 +1,39 @@
-class Structure01(var a:String, var b:Double){
-    constructor(a:String):this(a, 1.0){}
-    constructor(b:Double):this("Y", b){}
+class Structure00 {
+    constructor(param00: Int) {}
 }
 
-class Structure02(var a:String, var b:Double=1.0){
-    constructor(b:Double):this("X", b){}
+class Structure01 {
+    constructor(param00: Int) {}
+    constructor(param00: Int, param01: String) : this(param00) {}
 }
 
-class Structure03(var a:String, var b:Double, var c:Int=100){
-    constructor(a:String):this(a, 1.0){}
-    constructor(b:Double):this("X", b){}
-    constructor(a:String, c:Int):this(a, 1.0, c){}
-    constructor(b:Double, c:Int):this("X", b, c){}
+class Structure02 {
+    constructor(param00: Int) {}
+    constructor(param00: Int, param01: String) : this(param00) {}
+    constructor(param00: Int, param01: String, param02: Double) : this(param00, param01) {}
 }
 
-class Structure04(val a:String){
-    lateinit var b: String
-    constructor(a: String, b: String) : this(a) {this.b = b}
+class Structure03(param00: Int, param01: String="Hello", param02: Double = 3.14) {}
+
+class Structure04 {
+    lateinit var property00: String
+    constructor(param00: String) {property00 = param00}
 }
 
-Structure01("X")
-Structure01(b=3.0)
-Structure02("Y")
-Structure02(b=3.0)
-Structure03("Y")
-Structure03(b=3.0)
-Structure03(a="X", c=200)
-Structure03(b=3.0, c=200)
-Structure04("X")
-Structure04("X", "Y")
+class Structure05 {
+    lateinit var property00: String
+    constructor(param00: String) {this.property00 = param00}
+}
+
+Structure00(1)
+Structure01(0)
+Structure01(0, "Hello")
+Structure02(1)
+Structure02(1, "World")
+Structure02(1, "World", 3.14)
+Structure03(2)
+Structure03(2)
+Structure03(2, "World")
+Structure03(2, "World", 2.718)
+Structure04("Hello").property00
+Structure04("World").property00
