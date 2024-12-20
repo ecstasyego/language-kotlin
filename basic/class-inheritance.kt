a17f747bdb69396c59b92578d1be43cd18ba7b85
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-20 16:11:23 +0900

    Update class-inheritance.kt

--- basic/class-inheritance.kt
+++ basic/class-inheritance.kt
@@ -1,28 +1,41 @@
 open class Parent00
-class Child00:Parent00(){
+class P00Child00:Parent00(){
 }
+class P00Child01:Parent00{
+    constructor(): super(){}
+}
+
+
 
 open class Parent01{
     init{}
 }
-class Child01:Parent01(){
+class P01Child00:Parent01(){
     init{}
 }
+class P01Child01:Parent01{
+    init{}
+    constructor(): super(){}
+}
+
+
 
 open class Parent02{
     val property00:Int by lazy{1} 
     init{property00}
 }
-class Child02:Parent02(){
+class P02Child00:Parent02(){
     val property01:Int by lazy{1} 
     init{property01}
 }
+
+
 
 open class Parent03(param00:Int){
     val property00:Int by lazy{param00} 
     init{property00}
 }
-class Child03(param01:Int):Parent03(param01){
+class P03Child00(param01:Int):Parent03(param01){
     val property01:Int by lazy{param01}
     init{property01}
 }
