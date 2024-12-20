40d6a893bf66bb0982bdeb8f399a9da6b2a31768
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-20 13:30:16 +0900

    Create class-inheritance.kt

---
+++ basic/class-inheritance.kt
@@ -0,0 +1,18 @@
+open class Parent00
+class Child00:Parent00(){
+}
+
+open class Parent01{
+    init{}
+}
+class Child01:Parent01(){
+    init{}
+}
+
+open class Parent02{
+    val property00:Int by lazy{1} 
+    init{property00}
+}
+class Child02:Parent02(){
+    init{}
+}
