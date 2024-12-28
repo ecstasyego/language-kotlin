5d228bb7f07d2c3198165bad2a02ec5b95a25b81
ecstasyego <70621679+ecstasyego@users.noreply.github.com>
2024-12-28 16:41:26 +0900

    Update null-safety.kt

--- basic/null-safety.kt
+++ basic/null-safety.kt
@@ -1,1 +1,21 @@
+var variable00:Int = 1
+var variable01:Int? = 1
+var variable02:String = "Hello"
+var variable03:String? = null
+var variable04:String? = null ?: "World"
 
+// Safe Call(?.)
+true?.toString()
+null?.toString()
+
+
+// Elvis(?:)
+true?.toString() ?: 0
+null?.toString() ?: 0
+
+
+// Null-Check
+true == null
+false == null
+1 == null
+"Hello" == null
